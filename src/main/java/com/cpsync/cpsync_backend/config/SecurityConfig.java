483bc1f0327a43b89d8309bee54f2343873b1abc
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook-Air.local>
2026-06-22 14:51:13 +0530

    added cors config

--- src/main/java/com/cpsync/cpsync_backend/config/SecurityConfig.java
+++ src/main/java/com/cpsync/cpsync_backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://your-frontend-domain.com"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://cp-sync-frontend.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
